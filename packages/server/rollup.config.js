bec0299f8e4030a312c437f3a33d282d886158d5
mingdev <mingdev@163.com>
2025-07-29 20:17:45 +0800

    feat(server): 增加cjs的打包配置

--- packages/server/rollup.config.js
+++ packages/server/rollup.config.js
@@ -1,10 +1,10 @@
 const { getRollupConfig } = require('../../rollup.utils');
 
 module.exports = ({ packageDir }) => [
-  // getRollupConfig({
-  //   packageDir,
-  //   format: 'cjs',
-  // }),
+  getRollupConfig({
+    packageDir,
+    format: 'cjs',
+  }),
   getRollupConfig({
     packageDir,
     format: 'esm',
