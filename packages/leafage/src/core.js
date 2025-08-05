7244c4b60a2a03add53c0117ee5992cc6543ce1d
mingdev <mingdev@163.com>
2025-08-06 00:32:26 +0800

    feat(leafage): 增加src目录的导入识别

--- packages/leafage/src/core.js
+++ packages/leafage/src/core.js
@@ -6,6 +6,12 @@ export const createContext = (options) => {
   const hooks = createHooks();
   // eslint-disable-next-line no-use-before-define
   const callHook = (...args) => runWithContext(context, () => hooks.callHook(...args));
+  const resolvePaths = [
+    import.meta.url,
+    path.join(options.dir.root, options.dir.src),
+    options.dir.root,
+    path.join(options.dir.root, 'node_modules'),
+  ];
 
   const context = {
     version: process.env.PACKAGE_VERSION,
@@ -13,18 +19,10 @@ export const createContext = (options) => {
     hook: hooks.hook,
     callHook,
     resolveModule: (id) => imports.resolveModule(id, {
-      paths: [
-        import.meta.url,
-        options.dir.root,
-        path.join(options.dir.root, 'node_modules'),
-      ],
+      paths: resolvePaths,
     }),
     importModule: (id) => imports.importModule(id, {
-      paths: [
-        import.meta.url,
-        options.dir.root,
-        path.join(options.dir.root, 'node_modules'),
-      ],
+      paths: resolvePaths,
     }),
     close: () => callHook('close', context),
     runWithContext: (fn) => runWithContext(context, fn),
