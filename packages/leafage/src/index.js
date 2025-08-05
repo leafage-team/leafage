57359a76a76f7356a3d0dbae4d183820939a5cf1
mingdev <mingdev@163.com>
2025-08-05 22:11:56 +0800

    feat(leafage): 增加导出方法以及模块

--- packages/leafage/src/index.js
+++ packages/leafage/src/index.js
@@ -1,3 +1,4 @@
-export { loadConfig } from '@leafage/toolkit';
+export { loadConfig, logger } from '@leafage/toolkit';
 export { createServer } from '@leafage/server';
+export { bundle as build } from '@leafage/rspack';
 export { createContext } from './core';
