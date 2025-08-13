624d90a92b1db4a59edefa9d7bead95b5e951072
mingdev <mingdev@163.com>
2025-08-14 00:14:50 +0800

    feat(example): 增加脚本文件

--- examples/simple/bin/dev.js
+++ examples/simple/bin/dev.js
@@ -1,10 +1,13 @@
-const { createContext, loadConfig, logger, build } = require('leafage');
+const { createContext, loadConfig, createServer, logger, build } = require('leafage');
 
 const start = async () => {
   const options = await loadConfig();
   const context = createContext(options);
+  const server = createServer(context);
 
   await build(context);
+
+  server.start();
 };
 
 start().catch((error) => {
--- examples/simple/bin/start.js
+++ examples/simple/bin/start.js
@@ -1,10 +1,11 @@
-const { createContext, loadConfig, logger, build } = require('leafage');
+const { createContext, loadConfig, createServer, logger } = require('leafage');
 
 const start = async () => {
   const options = await loadConfig();
   const context = createContext(options);
+  const server = createServer(context);
 
-  await build(context);
+  server.start();
 };
 
 start().catch((error) => {
