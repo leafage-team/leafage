94dad14831ff77de3394da41d0ad6276033ac865
mingdev <mingdev@163.com>
2025-08-14 01:01:50 +0800

    feat(example): 增加标题显示

--- examples/simple/src/pages/home/index.jsx
+++ examples/simple/src/pages/home/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Helmet } from '@leafage/component';
 
-const HomePage = () => (<div>HomePage</div>);
+const HomePage = () => (
+  <>
+    <Helmet>
+      <title>home title</title>
+    </Helmet>
+    <div>HomePage</div>
+  </>
+);
 
 export default HomePage;
