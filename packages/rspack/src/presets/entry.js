cb7213fb0f1d68889ac50d788795a078535c44e0
mingdev <mingdev@163.com>
2025-08-22 11:41:16 +0800

    feat(rspack): 错误页面组件增加loader处理

--- packages/rspack/src/presets/entry.js
+++ packages/rspack/src/presets/entry.js
@@ -9,32 +9,40 @@ export const entryPreset = (ctx) => {
   const pattern = path.join(ctx.options.dir.root, ctx.options.dir.src, ctx.options.dir.page, ctx.options.dir.pattern);
   const { base } = globBase(pattern);
 
+  const entry = {
+    Error: getInnerComponentPath('Error', ctx.options),
+  };
+  if (ctx.isServer) {
+    const serverEntryPath = searchFileByName('server', ctx.options);
+    if (serverEntryPath) {
+      entry.server = serverEntryPath;
+    }
+
+    entry.Document = getInnerComponentPath('Document', ctx.options);
+    entry.App = getInnerComponentPath('App', ctx.options);
+  }
+  // page entry
   glob.sync(pattern).forEach((file) => {
     const name = path.relative(base, file).replace(path.extname(file), '');
+
+    entry[name] = file;
+  });
+  Object.keys(entry).reverse().forEach((name) => {
+    const file = entry[name];
     if (ctx.isServer) {
       ctx.config.entry[name] = file;
       return;
     }
 
-    const entryPath = `${CLIENT_ENTRY_LOADER}!${file}`;
+    const clientEntry = `${CLIENT_ENTRY_LOADER}!${file}`;
     if (ctx.isDev) {
-      ctx.config.entry[name] = [require.resolve('webpack-hot-middleware/client'), entryPath];
+      ctx.config.entry[name] = [require.resolve('webpack-hot-middleware/client'), clientEntry];
       return;
     }
 
-    ctx.config.entry[name] = entryPath;
+    ctx.config.entry[name] = clientEntry;
   });
 
-  ctx.config.entry.Error = getInnerComponentPath('Error', ctx.options);
-  if (ctx.isServer) {
-    const serverEntryPath = searchFileByName('server', ctx.options);
-    if (serverEntryPath) {
-      ctx.config.entry.server = serverEntryPath;
-    }
-
-    ctx.config.entry.Document = getInnerComponentPath('Document', ctx.options);
-    ctx.config.entry.App = getInnerComponentPath('App', ctx.options);
-  }
   if (ctx.isClient) {
     ctx.config.resolveLoader.alias[CLIENT_ENTRY_LOADER] = require.resolve('../loader/client-entry-loader');
   }
