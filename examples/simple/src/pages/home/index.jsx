e79209b8c890273cbacae85ede9725d355d2c091
mingdev <mingdev@163.com>
2025-08-25 15:29:02 +0800

    feat(example): 增加样式文件

--- examples/simple/src/pages/home/index.jsx
+++ examples/simple/src/pages/home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Helmet } from 'leafage/component';
 
+import styles from './style.module.scss';
+
 const HomePage = () => (
   <>
     <Helmet>
       <title>home title</title>
     </Helmet>
-    <div>HomePage</div>
+    <div className={styles.container}>HomePage</div>
   </>
 );
 
