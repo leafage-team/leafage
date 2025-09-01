a0cf1a14c55877ccff44b3acfd8cc898f9eb7c7e
mingdev <mingdev@163.com>
2025-09-01 16:54:45 +0800

    feat(toolkit): 修改配置的合并方式

--- packages/toolkit/src/config/index.js
+++ packages/toolkit/src/config/index.js
@@ -1,8 +1,16 @@
 import * as c12 from 'c12';
+import { createDefu } from 'defu';
 import common from './common';
 import builder from './builder';
 import server from './server';
 import { mergeProps } from '../utils';
+
+const merger = createDefu((obj, key, value) => {
+  if (Array.isArray(obj[key]) && Array.isArray(value)) {
+    obj[key] = obj[key].concat(value);
+    return true;
+  }
+});
 
 export const getConfig = (options = {}) => mergeProps(common, { builder, server }, options);
 export const loadConfig = async (options = {}) => {
@@ -13,6 +21,7 @@ export const loadConfig = async (options = {}) => {
         name: 'leafage',
         dotenv: false,
         defaults: getConfig(),
+        merger,
       },
     ),
   );
