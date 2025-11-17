856ae69a399c90f0f2e643f6d40ce48a590a4632
mingdev <mingdev@163.com>
2025-11-17 11:29:15 +0800

    feat(component): 修改组件的头部信息

--- packages/component/src/Error.jsx
+++ packages/component/src/Error.jsx
@@ -41,10 +41,6 @@ const Error = ({ statusCode, message }) => {
     <div style={styles.container}>
       <Helmet>
         <title>{title}</title>
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
-        />
       </Helmet>
       <div style={styles.wrapper}>
         <div style={styles.title}>{status}</div>
