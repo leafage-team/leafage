caa584e46a8af6f13e80001d497b0b3bb0bdf365
mingdev <mingdev@163.com>
2025-11-18 23:58:59 +0800

    feat(server): 修改设置请求头的方法

--- packages/server/src/presets/base.js
+++ packages/server/src/presets/base.js
@@ -1,7 +1,7 @@
 export const basePreset = (ctx) => {
   ctx.server.use((req, res, next) => {
     // 设置自定义版权
-    res.set('x-powered-by', `${process.env.PACKAGE_NAME}/${process.env.PACKAGE_VERSION}`);
+    res.setHeader('x-powered-by', `${process.env.PACKAGE_NAME}/${process.env.PACKAGE_VERSION}`);
 
     next();
   });
