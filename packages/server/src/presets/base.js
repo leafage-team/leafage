a3610fb90710f68813b9d88e5e79f8f9ddfcd894
周志明 <zhiming.zhou@tpsee8.com>
2025-12-10 11:24:59 +0800

    feat(server): 使用router做为服务

--- packages/server/src/presets/base.js
+++ packages/server/src/presets/base.js
@@ -1,6 +1,18 @@
+import bodyParser from 'body-parser';
+import parseUrl from 'parseurl';
+
 export const basePreset = (ctx) => {
+  // 解析json数据
+  ctx.server.use(bodyParser.json());
+  // 解析 application/x-www-form-urlencoded
+  ctx.server.use(bodyParser.urlencoded({ extended: false }));
   ctx.server.use((req, res, next) => {
-    // 设置自定义版权
+    // 设置pathname
+    req.pathname = parseUrl(req)?.pathname || '/';
+    // 设置默认params和query
+    req.params = req.params || {};
+    req.query = req.query || {};
+    // 设置版权
     res.setHeader('x-powered-by', `${process.env.PACKAGE_NAME}/${process.env.PACKAGE_VERSION}`);
 
     next();
--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,11 +1,15 @@
-import connect from 'connect';
+import http from 'node:http';
+import serverRouter from 'router';
+import finalhandler from 'finalhandler';
 import { utils } from '@leafage/toolkit';
 import { basePreset } from './presets/base';
 import { devPreset } from './presets/dev';
 import { staticPreset } from './presets/static';
 
 export const createServer = (context) => {
-  const server = connect();
+  const server = serverRouter();
+  const listener = http.createServer((req, res) => server(req, res, finalhandler(req, res)));
+  server.listen = listener.listen.bind(listener);
 
   context.callHook('server:create');
 
